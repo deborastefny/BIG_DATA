c92ec71f89e2bcbf56c4ef4396b9f4900a22b604
DEBORA STEFANY FREIRE ALVES DE LIMA <202503135801@alunos.unifanor.edu.br>
2025-08-27 20:24:49 -0300

    variaveis

---
+++ BIGDATA1/VARIAVEIS/exemplo_001.py
@@ -0,0 +1,15 @@
+# criação da primeira variavel sem estabelecer o tipo
+
+
+nome='paulo'
+print(nome)
+# o codigo acima cria uma variavel chamada nome e atribui a ela o valor paulo
+
+idade=20
+print(idade)
+# o codigo acima cria uma variavel chamada idade e atribui a ela o valor 20
+
+#print(nome,idade)
+
+#imprimir variaveis usando f-string
+print(f'O nome é {nome} e a idade é {idade} anos')
